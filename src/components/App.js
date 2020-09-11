9a50790b41129cb60d6bcccabbc061a5b4569d8f
Kurtis Young <kurtiscyoung@gmail.com>
2020-09-11 12:04:18 -0700

    Write first test for App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <img className="logo" src="./assets/marvel-logo-header.jpg" alt="Marvel"/>
-      <h1 className="title">Who is your favourite <span className="hero-text">hero</span> or <span className="villian-text">villian</span>?</h1>
+      <h1 className="title">Are you a <span className="hero-text">hero</span> or a <span className="villian-text">villian</span>?</h1>
       <Slider/>
     </div>
   );
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders header and img elements', () => {
+  const { getByText, getByAltText } = render(<App />);
+  const headerElement = getByText(/Are you a/i);
+  const imgElement = getByAltText(/Marvel/i);
+  expect(headerElement).toBeInTheDocument();
+  expect(imgElement).toBeInTheDocument();
 });
