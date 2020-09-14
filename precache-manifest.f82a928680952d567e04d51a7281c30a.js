1d28448e428eb108cee24353dbd888424e3c74a5
Kurtis Young <kurtiscyoung@gmail.com>
2020-09-13 21:12:45 -0700

    Updates

---
+++ precache-manifest.f82a928680952d567e04d51a7281c30a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e879b963c3c76d40ea7183aa8e1199",
+    "url": "/react-slider-app/index.html"
+  },
+  {
+    "revision": "c27f718275fe2a407759",
+    "url": "/react-slider-app/static/css/2.2eb7cac7.chunk.css"
+  },
+  {
+    "revision": "83f05afe1fedb60ce0e5",
+    "url": "/react-slider-app/static/css/main.86379fcd.chunk.css"
+  },
+  {
+    "revision": "c27f718275fe2a407759",
+    "url": "/react-slider-app/static/js/2.dc3a75b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-slider-app/static/js/2.dc3a75b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f05afe1fedb60ce0e5",
+    "url": "/react-slider-app/static/js/main.c7a95b27.chunk.js"
+  },
+  {
+    "revision": "56cadd9a4d0e110a417c",
+    "url": "/react-slider-app/static/js/runtime-main.6c2846d1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-slider-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-slider-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-slider-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-slider-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/api/marvel.js
+++
@@ -1,3 +0,0 @@
-export function fetchAPI() {
-  return fetch(`https://gateway.marvel.com:443/v1/public/characters?orderBy=-modified&limit=30&apikey=${process.env.REACT_APP_API_KEY}`)
-}
